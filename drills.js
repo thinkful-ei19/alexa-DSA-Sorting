2d71829325871bd4f5869827fbfe774a3f61f5fb
Alexa Scott <arscott4792@gmail.com>
2018-05-24 11:41:57 -0700

    added counters for qSort and mSort - need to check

--- drills.js
+++ drills.js
@@ -2,10 +2,18 @@
 
 const data = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
 
-///////// QUICK SORT //////////
+///////////////////////////// QUICK SORT /////////////////////////////
+
+// qSort counter
+let qCounter = 0;
+// partition counter
+let pCounter = 0;
+// swaps counter
+let sCounter = 0;
 
 // swaps the values at two indices in an array
 function swap(array, i, j) {
+  sCounter++;
   const tmp = array[i];
   array[i] = array[j];
   array[j] = tmp;
@@ -15,6 +23,7 @@ function swap(array, i, j) {
 // loop through array, swapping values as you go to put them on either side of pivot
 // put pivot into correct place in array
 function partition(array, start, end) {
+  pCounter++;
   let compares = 0;
   let swaps = 0;
   const pivot = array[end - 1];
@@ -40,6 +49,7 @@ function partition(array, start, end) {
 // values > pivot --> go on other half of array
 // then, recursively sort two halves of the array until halves are length 0 or 1
 function qSort(array, start=0, end=array.length) {
+  qCounter++;
   start = start;
   end = end;
  
@@ -54,14 +64,31 @@ function qSort(array, start=0, end=array.length) {
 }
 
 // console.log(qSort(data));
+qSort(data);
+
+function quickSortCounter() {
+  console.log('this is qCounter:', qCounter);
+  console.log('this is pCounter:', pCounter);
+  console.log('this is sCounter:', sCounter);
+  const totalCounter = qCounter + pCounter + sCounter;
+  console.log('this is totalCounter:', totalCounter);
+}
+
+// quickSortCounter();
 
 
-///////// MERGE SORT //////////
+///////////////////////////// MERGE SORT /////////////////////////////
 
-// to merge, keep choosinf lowest value from left or right arrays
+// to merge, keep choosing lowest value from left or right arrays
 // which hasnt already been added to the output
 // when one array is empty, then just add all of the remaining values from the other to the array
+
+let mergeCounter = 0;
+let mergeSortCounter = 0;
+
 function merge(left, right, array) {
+  mergeCounter++;
+
   let leftIndex = 0;
   let rightIndex = 0;
   let outputIndex = 0;
@@ -90,10 +117,12 @@ function merge(left, right, array) {
 // then merges them back together in the correct order
 
 // if array has 0 or 1 elements, then its sorted
-//slice array into two halves
+// slice array into two halves
 // sort each half by recursively calling mSort
 // two sorted halves are then merged togethr in correct order using merge function
 function mSort(array) {
+  mergeSortCounter++;
+
   if (array.length <= 1) {
     return array;
   }
@@ -107,4 +136,17 @@ function mSort(array) {
   return merge(left, right, array);
 }
 
-console.log(mSort(data));
+// console.log(mSort(data));
+mSort(data);
+
+function mergeSortCounterTotal() {
+  console.log('this is mergeCounter:', mergeCounter);
+  console.log('this is mergeSortCounter:', mergeSortCounter);
+  const mergeSortTotalCounter = mergeCounter + mergeSortCounter;
+  console.log('this is mergeSortTotalCounter:', mergeSortTotalCounter);
+}
+
+mergeSortCounterTotal();
+
+
+///////////////////////////// BUCKET SORT /////////////////////////////
